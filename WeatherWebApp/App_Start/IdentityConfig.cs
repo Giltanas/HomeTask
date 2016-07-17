1fbbc39d446daeb59e3b1caab13fb415133b781d
Giltanas <giltanas94@gmail.com>
2016-07-17 14:50:24 +0300

    ENHANCEMENT: Made IdentityConfig for Owin and changed Web.Config for it

---
+++ WeatherWebApp/App_Start/IdentityConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using WeatherWebApp.Context;
+using WeatherWebApp.Managers;
+using WeatherWebApp.Models;
+
+[assembly: OwinStartup(typeof(WeatherWebApp.IdentityConfig))]
+
+namespace WeatherWebApp
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(() => new UserDbContext());
+            app.CreatePerOwinContext<AppUserManager>(AppUserManager.Create);
+            app.CreatePerOwinContext<RoleManager<AppRole>>((options, context) =>
+                new RoleManager<AppRole>(
+                    new RoleStore<AppRole>(context.Get<UserDbContext>())));
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Home/Login"),
+            });
+        }
+    }
+}
