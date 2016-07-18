7ef149229466e0f3d0ab767991664633285d8552
Giltanas <giltanas94@gmail.com>
2016-07-18 13:05:44 +0300

    FIX: User logs fix

--- WeatherWebApp/Models/User.cs
+++ WeatherWebApp/Models/User.cs
@@ -13,21 +13,26 @@ namespace WeatherWebApp.Models
     public class User : IdentityUser
     {
         public virtual ICollection<City> Cities { get; set; }
-        public List<Log> Logs = new List<Log>();
-        
+        public virtual List<Log> Logs { get; set; }
+
         public void AddLog(string cityName)
         {
+            if (Logs == null)
+            {
+                Logs = new List<Log>();
+            }
             var log = new Log() { CityName = cityName, Date = DateTime.Now };
             
          //   Request.GetOwinContext().Get<WeatherContext>().Entry(user).State = EntityState.Detached;
 
             using (var db = new WeatherContext())
             {
-                db.Users.Attach(this);
                 db.Logs.Attach(log);
+                db.Users.Attach(this);            
 
                 db.Logs.Add(log);
                 Logs.Add(log);
+
                 log.User = this;
                 db.SaveChanges();
             }
